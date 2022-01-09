63369d791d4ffe19ef377919dad92cef1788133e
annyts <90929385+annyts@users.noreply.github.com>
2022-01-09 23:35:39 +0400

    edit

---
+++ form.js
@@ -0,0 +1,13 @@
+function slaider(){
+    let slide = document.querySelector(".slider img");
+    slide.src = imagesslides[n];
+  
+    if(n < imagesslides.length - 1){
+      n=n+1; 
+    }
+    else { 
+      n = 0;
+    }
+let imagesslides = ["image/slider/სლაიდერი1.png", "image/slider/სლაიდერი2.png", "image/slider/სლაიდერი3.png"]
+let n = 1;
+let t = 5000;
